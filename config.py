48f767d9410f31313fd4ade5b4dca66a757c9a8a
Gauri Chaudhari <chaudharigauri.9@gmail.com>
2024-01-10 19:05:46 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = {
-    'user': 'sql5669038',
-    'password': 'bl78kBRCdA',
-    'host': 'sql5.freemysqlhosting.net',
+    'user': 'id21775167_admin',
+    'password': 'Gauridimple@9',
+    'host': '99.000webhost.io',
     'port': '3306',
-    'database': 'sql5669038',
+    'database': 'id21775167_datawage',
     'raise_on_warnings': True
 }
